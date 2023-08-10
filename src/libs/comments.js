09a2da780f32548e3d87ebe66c8752f64c96ac14
FAH <apichaya.inkhiewsai@gmail.com>
2023-08-10 15:21:03 +0700

    4

--- src/libs/comments.js
+++ src/libs/comments.js
@@ -23,7 +23,7 @@ export const comments = [
     username: "Charlie Brown",
     userImagePath: "/profileImages/charliebrown.jpg",
     commentText: "บ้าไปแล้ว",
-    likeNum: 4,
+    likeNum: 0,
     replies: [],
   },
 ];
